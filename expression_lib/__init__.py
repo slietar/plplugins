2dd68da107a331796a242ed8ed3550f3610d0f43
Simon Liétar <simon@lietar.net>
2026-01-08 01:52:04 +0100

    Improve implode_like

--- expression_lib/__init__.py
+++ expression_lib/__init__.py
@@ -1,6 +1,5 @@
 # expression_lib/__init__.py
 from pathlib import Path
-from typing import TYPE_CHECKING
 
 import polars as pl
 from polars._typing import IntoExpr
@@ -9,21 +8,12 @@ from polars.plugins import register_plugin_function
 
 PLUGIN_PATH = Path(__file__).parent
 
-def pig_latinnify(expr: IntoExpr) -> pl.Expr:
-    """Pig-latinnify expression."""
-    return register_plugin_function(
-        plugin_path=PLUGIN_PATH,
-        function_name="pig_latinnify",
-        args=expr,
-        is_elementwise=True,
-    )
-
 
 def implode_like(target_expr: IntoExpr, layout_expr: IntoExpr) -> pl.Expr:
     """Implode `target_expr` like the layout of `layout_expr`."""
     return register_plugin_function(
         plugin_path=PLUGIN_PATH,
-        function_name="implode_like",
+        function_name="implode_like_expr",
         args=(target_expr, layout_expr),
         is_elementwise=False,
     )
--- src/lib.rs
+++ src/lib.rs
@@ -1,77 +1,51 @@
 use polars::prelude::*;
-use pyo3_polars::{derive::polars_expr, export::{polars_arrow::array::ListArray, polars_core::utils::align_chunks_binary}};
-use std::fmt::Write;
+use pyo3_polars::export::polars_core::utils::align_chunks_binary;
+use pyo3_polars::export::polars_arrow::array::ListArray;
+use pyo3_polars::derive::polars_expr;
 
 
-fn pig_latin_str(value: &str, output: &mut String) {
-    if let Some(first_char) = value.chars().next() {
-        write!(output, "{}{}ay", &value[1..], first_char).unwrap()
-    }
-}
+fn implode_like(
+    target_series: &Series,
+    layout_series: &Series,
+) -> PolarsResult<Series> {
+    let target_ca = target_series.unpack::<Float32Type>()?;
+    let layout_ca = layout_series.list()?;
 
-#[polars_expr(output_type=String)]
-fn pig_latinnify(inputs: &[Series]) -> PolarsResult<Series> {
-    let ca = inputs[0].str()?;
-    let out: StringChunked = ca.apply_into_string_amortized(pig_latin_str);
-    Ok(out.into_series())
-    // x
-}
-
-// #[pyo3::pyfunction]
-#[polars_expr(output_type=Float32)]
-fn implode_like(inputs: &[Series]) -> PolarsResult<Series>{
-    let target_list = inputs[0].f32()?;
-    let layout_list = inputs[1].list()?;
-
-    let (lhs, rhs) = align_chunks_binary(target_list, layout_list);
-
-    let offsets = layout_list.offsets()?;
-    let offsets_buffer = offsets.buffer();
-
-    let start = offsets[0] as usize;
-    let len = offsets[offsets.len() - 1] as usize - start;
-
-    let target_chunks = target_list.rechunk().into_owned();
-    let target_array = &target_chunks.chunks()[0];
-
-    let new_array = ListArray::new(
-        // target_array.dtype().clone(),
-        ArrowDataType::LargeList(
-            Box::new(polars_arrow::datatypes::Field::new(
-                "item".into(),
-                ArrowDataType::Float32,
-                true,
-            ))
-        ),
-        offsets,
-        target_array.clone(),
-        None,
+    let (target_aligned_ca, layout_aligned_ca) = align_chunks_binary(
+        target_ca,
+        layout_ca,
     );
 
-    let new_chunked = unsafe { ListChunked::from_chunks("foo".into(), vec![Box::new(new_array)]) };
-    let new_series = new_chunked.into_series();
+    let new_chunks_iter = target_aligned_ca
+        .chunks()
+        .iter()
+        .zip(
+            layout_aligned_ca.downcast_iter()
+        )
+        .map(|(target_chunk, layout_chunk)| {
+            ListArray::new(
+                DataType::List(
+                    Box::new(
+                        target_ca.dtype().clone()
+                    )
+                ).to_arrow(CompatLevel::newest()),
+                layout_chunk.offsets().clone(),
+                target_chunk.clone(),
+                None,
+            )
+        });
 
-    Ok(new_series)
+    let new_ca = ListChunked::from_chunk_iter(
+        target_series.name().clone(),
+        new_chunks_iter,
+    );
 
-    // ChunkedArray::<ListType>::from
-    // let _ = unsafe {
-    //     ListChunked::from_chunks("Foo", vec![
-    //         Box::new(0)
-    //     ]);
-    // }
+    Ok(new_ca.into_series())
+}
 
-    // ChunkedArray::set_fast_explode_list(&mut self, value);
 
-    // Create new series with data from inputs[0] and offsets from inputs[1]
 
-    // align_chunks_binary(target_list, offsets)
-
-    // let x = &lhs.chunks()[0];
-    // x.off
-
-            // .zip(rhs.iter())
-            // .map(|(lhs, rhs)| match (lhs, rhs) {
-            //     (Some(lhs), Some(rhs)) => f(&lhs, &rhs),
-            //     _ => None,
-            // });
+#[polars_expr(output_type=Float32)]
+fn implode_like_expr(inputs: &[Series]) -> PolarsResult<Series>{
+    implode_like(&inputs[0], &inputs[1])
 }
