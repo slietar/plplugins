310061adaf643803e6a6838cc0ae7813d1b7789a
Simon Liétar <simon@lietar.net>
2026-01-14 21:46:56 +0100

    Add more functions

--- src/functions.rs
+++ src/functions.rs
@@ -1,74 +1,98 @@
 use polars::prelude::*;
+use pyo3_polars::export::polars_arrow::array::ListArray;
 use pyo3_polars::export::polars_arrow::buffer::Buffer;
 use pyo3_polars::export::polars_arrow::offset::OffsetsBuffer;
-use pyo3_polars::export::polars_core::utils::{Container, align_chunks_binary_ca_series};
-use pyo3_polars::export::polars_arrow::array::ListArray;
+use pyo3_polars::export::polars_core::utils::{align_chunks_binary_ca_series, Container};
 
+pub fn get_offsets(series: &Series) -> PolarsResult<Series> {
+    let list = series.list()?;
+    let name = series.name().clone();
 
-pub fn implode_like(
-    target_series: &Series,
-    layout_series: &Series,
-) -> PolarsResult<Series> {
+    if series.n_chunks() == 1 {
+        let first_chunk = list.downcast_iter().next().unwrap();
+        let offsets = first_chunk.offsets();
+
+        if offsets[0] == 0 {
+            return Ok(Series::new(name, offsets.as_slice()));
+        }
+    }
+
+    let mut offsets = Series::from_vec(name, vec![0]);
+
+    offsets.append(&cum_sum(
+        &Series::new(PlSmallStr::EMPTY, list.lst_lengths()),
+        false,
+    )?)?;
+
+    Ok(offsets)
+}
+
+pub fn implode_like(target_series: &Series, layout_series: &Series) -> PolarsResult<Series> {
     let layout_ca = layout_series.list()?;
 
-    let (layout_aligned_ca, target_aligned_series) = align_chunks_binary_ca_series(
-        layout_ca,
-        target_series,
-    );
+    let (layout_aligned_ca, target_aligned_series) =
+        align_chunks_binary_ca_series(layout_ca, target_series);
 
     if layout_ca.inner_length() != target_series.len() {
-        return Err(
-            PolarsError::ShapeMismatch(
-                format!(
-                    "Target series length ({}) does not match layout inner length ({})",
-                    target_series.len(),
-                    layout_ca.inner_length(),
-                ).into()
+        return Err(PolarsError::ShapeMismatch(
+            format!(
+                "Target series length ({}) does not match layout inner length ({})",
+                target_series.len(),
+                layout_ca.inner_length(),
             )
-        );
+            .into(),
+        ));
     }
 
     let new_chunks_iter = target_aligned_series
         .chunks()
         .iter()
-        .zip(
-            layout_aligned_ca.downcast_iter()
-        )
+        .zip(layout_aligned_ca.downcast_iter())
         .map(|(target_chunk, layout_chunk)| {
             let offsets_source = layout_chunk.offsets();
 
             let offsets = if offsets_source[0] != 0 {
                 unsafe {
-                    OffsetsBuffer::new_unchecked(
-                        Buffer::from_iter(
-                            offsets_source.iter().map(|offset| offset - offsets_source[0])
-                        )
-                    )
+                    OffsetsBuffer::new_unchecked(Buffer::from_iter(
+                        offsets_source
+                            .iter()
+                            .map(|offset| offset - offsets_source[0]),
+                    ))
                 }
             } else {
                 offsets_source.clone()
             };
 
             ListArray::new(
-                DataType::List(
-                    Box::new(
-                        target_series.dtype().clone()
-                    )
-                ).to_arrow(CompatLevel::newest()),
+                DataType::List(Box::new(target_series.dtype().clone()))
+                    .to_arrow(CompatLevel::newest()),
                 offsets,
                 target_chunk.clone(),
                 layout_chunk.validity().cloned(),
             )
         });
 
-    let new_ca = ListChunked::from_chunk_iter(
-        target_series.name().clone(),
-        new_chunks_iter,
-    );
+    let new_ca = ListChunked::from_chunk_iter(target_series.name().clone(), new_chunks_iter);
 
     Ok(new_ca.into_series())
 }
 
+pub fn implode_with_lengths(
+    target_series: &Series,
+    lengths_series: &Series,
+) -> PolarsResult<Series> {
+    if lengths_series.has_nulls() {
+        return Err(PolarsError::ComputeError(
+            "Lengths series must not contain null values".into(),
+        ));
+    }
+
+    let mut offsets = Series::from_vec(PlSmallStr::EMPTY, vec![0i64]);
+
+    offsets.append(&cum_sum(&lengths_series, false)?)?;
+
+    implode_with_offsets(target_series, &offsets)
+}
 
 // See also: trim_lists_to_normalized_offsets()
 // https://github.com/pola-rs/polars/blob/778dbb645ccbff8b1e5999a279037571a03c718b/crates/polars-compute/src/trim_lists_to_normalized_offsets.rs#L9
@@ -81,73 +105,29 @@ pub fn implode_with_offsets(
     let offsets_ca = cast_offsets_series.i64().unwrap();
 
     if offsets_ca.first().unwrap() != 0 {
-        return Err(
-            PolarsError::ShapeMismatch(
-                "Offsets must start at zero".into(),
-            )
-        );
+        return Err(PolarsError::ShapeMismatch(
+            "Offsets must start at zero".into(),
+        ));
     }
 
-    let (offsets_aligned_ca, target_aligned_series) = align_chunks_binary_ca_series(offsets_ca, target_series);
+    let (offsets_aligned_ca, target_aligned_series) =
+        align_chunks_binary_ca_series(offsets_ca, target_series);
 
     let new_chunks_iter = target_aligned_series
         .chunks()
         .iter()
-        .zip(
-            offsets_aligned_ca.downcast_iter()
-        )
+        .zip(offsets_aligned_ca.downcast_iter())
         .map(|(target_chunk, offsets_chunk)| {
             ListArray::new(
-                DataType::List(
-                    Box::new(
-                        target_series.dtype().clone()
-                    )
-                ).to_arrow(CompatLevel::newest()),
+                DataType::List(Box::new(target_series.dtype().clone()))
+                    .to_arrow(CompatLevel::newest()),
                 unsafe { OffsetsBuffer::new_unchecked(offsets_chunk.values().clone()) },
                 target_chunk.clone(),
                 offsets_chunk.validity().cloned(),
             )
         });
 
-    let new_ca = ListChunked::from_chunk_iter(
-        target_series.name().clone(),
-        new_chunks_iter,
-    );
+    let new_ca = ListChunked::from_chunk_iter(target_series.name().clone(), new_chunks_iter);
 
     Ok(new_ca.into_series())
-}
-
-
-pub fn get_offsets(series: &Series) -> PolarsResult<Series> {
-    let list = series.list()?;
-    let name = series.name().clone();
-
-    if series.n_chunks() == 1 {
-        let ca = list;
-        let first_chunk = ca.downcast_iter().next().unwrap();
-        let offsets = first_chunk.offsets();
-
-        if offsets[0] == 0 {
-            return Ok(
-                Series::new(
-                    name,
-                    offsets.as_slice(),
-                )
-            );
-        }
-    }
-
-    let mut offsets = Series::from_vec(name, vec![0]);
-
-    offsets.append(
-        &cum_sum(
-            &Series::new(
-                PlSmallStr::EMPTY,
-                list.lst_lengths(),
-            ),
-            false,
-        )?
-    )?;
-
-    Ok(offsets)
 }
--- src/lib.rs
+++ src/lib.rs
@@ -2,50 +2,71 @@ mod functions;
 
 use polars::prelude::*;
 use pyo3::types::{PyModule, PyModuleMethods as _};
-use pyo3::{Bound, PyResult, Python, pyfunction, pymodule};
-use pyo3_polars::PySeries;
-use pyo3_polars::error::PyPolarsErr;
+use pyo3::{pyfunction, pymodule, Bound, PyResult, Python};
 use pyo3_polars::derive::polars_expr;
-
+use pyo3_polars::error::PyPolarsErr;
+use pyo3_polars::PySeries;
 
 #[pyfunction]
 fn get_offsets(series: PySeries) -> PyResult<PySeries> {
-    Ok(
-        PySeries(
-            functions::get_offsets(&series.into()).map_err(PyPolarsErr::from)?
-        )
-    )
+    Ok(PySeries(
+        functions::get_offsets(&series.into()).map_err(PyPolarsErr::from)?,
+    ))
 }
 
 #[pyfunction]
-fn implode_like(
-    target_series: PySeries,
-    layout_series: PySeries,
-) -> PyResult<PySeries> {
-    Ok(
-        PySeries(
-            functions::implode_like(
-                &target_series.into(),
-                &layout_series.into(),
-            ).map_err(PyPolarsErr::from)?
-        )
-    )
+fn implode_like(target_series: PySeries, layout_series: PySeries) -> PyResult<PySeries> {
+    Ok(PySeries(
+        functions::implode_like(&target_series.into(), &layout_series.into())
+            .map_err(PyPolarsErr::from)?,
+    ))
 }
 
+#[pyfunction]
+fn implode_with_lengths(target_series: PySeries, lengths_series: PySeries) -> PyResult<PySeries> {
+    Ok(PySeries(
+        functions::implode_with_lengths(&target_series.into(), &lengths_series.into())
+            .map_err(PyPolarsErr::from)?,
+    ))
+}
+
+#[pyfunction]
+fn implode_with_offsets(target_series: PySeries, offsets_series: PySeries) -> PyResult<PySeries> {
+    Ok(PySeries(
+        functions::implode_with_offsets(&target_series.into(), &offsets_series.into())
+            .map_err(PyPolarsErr::from)?,
+    ))
+}
 
 #[pymodule]
 fn plutils(_py: Python, m: &Bound<PyModule>) -> PyResult<()> {
     m.add_function(pyo3::wrap_pyfunction!(get_offsets, m)?)?;
     m.add_function(pyo3::wrap_pyfunction!(implode_like, m)?)?;
+    m.add_function(pyo3::wrap_pyfunction!(implode_with_lengths, m)?)?;
+    m.add_function(pyo3::wrap_pyfunction!(implode_with_offsets, m)?)?;
     Ok(())
 }
 
-
-fn implode_like_output(input_fields: &[Field]) -> PolarsResult<Field> {
+fn implode_output(input_fields: &[Field]) -> PolarsResult<Field> {
     Ok(input_fields[0].clone())
 }
 
-#[polars_expr(output_type_func=implode_like_output)]
-fn implode_like_expr(inputs: &[Series]) -> PolarsResult<Series>{
+#[polars_expr(output_type=Int64)]
+fn get_offsets_expr(inputs: &[Series]) -> PolarsResult<Series> {
+    functions::get_offsets(&inputs[0])
+}
+
+#[polars_expr(output_type_func=implode_output)]
+fn implode_like_expr(inputs: &[Series]) -> PolarsResult<Series> {
     functions::implode_like(&inputs[0], &inputs[1])
+}
+
+#[polars_expr(output_type_func=implode_output)]
+fn implode_with_lengths_expr(inputs: &[Series]) -> PolarsResult<Series> {
+    functions::implode_with_lengths(&inputs[0], &inputs[1])
+}
+
+#[polars_expr(output_type_func=implode_output)]
+fn implode_with_offsets_expr(inputs: &[Series]) -> PolarsResult<Series> {
+    functions::implode_with_offsets(&inputs[0], &inputs[1])
 }
